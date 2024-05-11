1a2f5ae2f1aa7c9f240bfdae8201d00663aa2559
TallesAndrade <tallesandrade7@gmail.com>
2024-05-10 23:56:20 -0300

    Projeto Finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por Favor informe os dados Solicitados!");
+
+        System.out.println("Nome Completo");
+        String nomeCompleto = scanner.nextLine();
+
+        System.out.println("Agencia:");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Conta:");
+        String conta = scanner.next();
+
+        System.out.println("Saldo:");
+        double saldo = scanner.nextDouble();
+        scanner.close();
+
+        System.out.println("Olá "+nomeCompleto+", obrigado por criar a sua conta em nosso banco, sua agência é "+agencia+", conta "+conta+" e seu saldo de R$ "+saldo+" já está disponivel para saque.");
 
     }
 }
